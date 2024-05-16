8cfac5edb79461b58b9c8f62f4ed740de547f2f9
Walter-DEL <57088085+Walter-DEL@users.noreply.github.com>
2024-05-16 19:48:13 +0800

    Update random_player.rb

--- random_player.rb
+++ random_player.rb
@@ -1,11 +1,28 @@
 require './base_player.rb'
 
 class RandomPlayer < BasePlayer
+  def initialize(game:, name:)
+    super
+    @visited = {}
+  end
+
   def next_point(time:)
-    move = {
-      row: rand(0..grid.max_row),
-      col: rand(0..grid.max_col)
-    }
+    # 获取所有尚未访问过的点
+    unvisited_points = grid.points.keys - @visited.keys
+
+    # 如果所有的点都已经访问过，那么选择一个随机点
+    if unvisited_points.empty?
+      move = {
+        row: rand(0..grid.max_row),
+        col: rand(0..grid.max_col)
+      }
+    else
+      # 否则，随机选择一个尚未访问过的点
+      move = unvisited_points.sample
+    end
+
+    # 标记选择的点为已访问
+    @visited[move] = true
 
     puts "Player #{name} at #{time} to #{move}"
 
