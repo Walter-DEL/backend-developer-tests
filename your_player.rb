554209ee8f6e13b503102f6484857f6c8a866b34
Walter-DEL <57088085+Walter-DEL@users.noreply.github.com>
2024-05-16 19:47:10 +0800

    Update your_player.rb

--- your_player.rb
+++ your_player.rb
@@ -1,19 +1,27 @@
-# your_player.rb
-
 require './base_player.rb'
 
 class YourPlayer < BasePlayer
-  def next_point(time:)
-    # 获取当前玩家的位置
-    current_position = game.current_position(self)
+  def initialize(game:, name:)
+    super
+    @visited = {}
+  end
 
-    # 获取所有相邻的点
+  def next_point(time:)
+    current_position = game.current_position(self)
     adjacent_points = grid.adjacent_points(current_position)
 
-    # 选择下一个要访问的点（示例：随机选择一个相邻点）
-    next_point = adjacent_points.sample
+    # 选择一个尚未访问过的相邻点
+    next_point = adjacent_points.find { |point| !@visited[point] }
 
-    # 返回坐标
+    # 如果所有相邻点都已经访问过，那么选择一个尚未访问过的点，该点与当前位置的距离最近
+    if next_point.nil?
+      unvisited_points = grid.points.keys - @visited.keys
+      next_point = unvisited_points.min_by { |point| grid.distance(current_position, point) }
+    end
+
+    # 标记选择的点为已访问
+    @visited[next_point] = true
+
     next_point
   end
 
